86289f032766fd3d8b427d1fc827afbc36db19b2
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-18 15:03:07 -0300

    improve: increased errorHandler handling scopes

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,8 +1,15 @@
 import { ErrorRequestHandler } from 'express';
+import codes from 'http-status-codes';
+import RequestError from '../utils/RequestError';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
-  res.status(500).json(error);
+  if (error instanceof RequestError) {
+    const { statusCode, message } = error;
+    return res.status(statusCode).json({ requestError: message });
+  }
+
+  return res.status(codes.INTERNAL_SERVER_ERROR).json(error);
 };
 
 export default errorHandler;
