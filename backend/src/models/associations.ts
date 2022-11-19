852807e0790ace1bed68f66b152699169e5218db
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-19 14:56:31 -0300

    feat: created an associations.ts file it minimizes clutter in model classes and abstracts association rules

---
+++ backend/src/models/associations.ts
@@ -0,0 +1,50 @@
+import Account from '../database/models/account';
+import Transaction from '../database/models/transaction';
+import User from '../database/models/user';
+
+export const userAccount = {
+  model: Account,
+  as: 'account',
+};
+
+export const accountOwner = {
+  model: User,
+  as: 'owner',
+  attributes: { exclude: ['accountId', 'password'] },
+};
+
+export const cashInAccount = {
+  model: Account,
+  as: 'cashInAccount',
+  include: [
+    { ...accountOwner },
+  ],
+  attributes: { exclude: ['balance'] },
+};
+
+export const cashOutAccount = {
+  model: Account,
+  as: 'cashOutAccount',
+  include: [
+    { ...accountOwner },
+  ],
+  attributes: { exclude: ['balance'] },
+};
+
+export const cashInTransactions = {
+  model: Transaction,
+  as: 'cashInTransactions',
+  attributes: { exclude: ['creditedAccount', 'debitedAccount'] },
+  include: [
+    cashOutAccount,
+  ],
+};
+
+export const cashOutTransactions = {
+  model: Transaction,
+  as: 'cashOutTransactions',
+  attributes: { exclude: ['debitedAccount', 'creditedAccount'] },
+  include: [
+    cashInAccount,
+  ],
+};
