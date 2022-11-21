2bf89f1e3f4a060f612f461d0581e57c4fa04e5c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-20 22:13:54 -0300

    feat: created authentication middleware

---
+++ backend/src/middlewares/authenticate.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import RequestError from '../utils/RequestError';
+import Token from '../utils/Token';
+
+const authentication: RequestHandler = (req, _res, next) => {
+  const { headers: { authorization } } = req;
+
+  if (!authorization) throw RequestError.unauthorized('Token not found.');
+
+  const { id } = Token.validate('authorization') as JwtPayload;
+  req.headers.id = id;
+  return next();
+};
+
+export default authentication;
