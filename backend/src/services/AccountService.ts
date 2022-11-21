b5c79d73522c3ee4464b8a1995267c0436f623eb
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-20 23:44:05 -0300

    feat: added verifications  to account creation

--- backend/src/services/AccountService.ts
+++ backend/src/services/AccountService.ts
@@ -4,8 +4,14 @@ import User from '../database/models/user';
 import RequestError from '../utils/RequestError';
 
 class AccountService {
-  static async createAccount(account: AccountDTO) {
-    return Account.create(account);
+  private static _model = new AccountModel();
+
+  /** Creates a NG Cash Account
+   * @param account if not provided, new account will have balance value 0;
+  */
+  static async createOne(account: AccountDTO = { balance: 0 }): Promise<AccountDTO> {
+    if (account.id) throw RequestError.conflict('Cannot provide ID in Account creation.');
+    return this._model.createOne(account);
   }
 
   static async findAll(): Promise<AccountDTO[]> {
