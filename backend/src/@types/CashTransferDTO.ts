0bd57000dfe0eec0490f23585d6a016cb510b6e8
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-20 21:42:14 -0300

    refactor: remover userId from cash transfer DTO the sender id and username come from the token payload

--- backend/src/@types/CashTransferDTO.ts
+++ backend/src/@types/CashTransferDTO.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod';
 
 export const cashTransferSchema = z.object({
-  userId: z.string().uuid(),
-  receiverUsername: z.string(),
+  creditedUsername: z.string(),
   value: z.number().min(0.01),
 });
 
