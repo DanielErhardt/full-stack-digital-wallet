acfcd74e3ce824a537f7db0e0ad371469e626109
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-23 11:17:35 -0300

    refactor: renamed sequelize config env variables

--- backend/src/database/config/index.ts
+++ backend/src/database/config/index.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  host: process.env.HOSTNAME || 'localhost',
-  port: Number(process.env.DATABASE_PORT) || 5432,
-  database: process.env.POSTGRES_DB || 'ng-cash',
-  username: process.env.POSTGRES_USER || 'admin',
-  password: process.env.POSTGRES_PASSWORD || 'admin',
+  host: process.env.DB_HOST || 'database',
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME || 'ng-cash',
+  username: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'admin',
   dialect: 'postgres',
   dialectOptions: {
     rejectUnauthorized: false,
