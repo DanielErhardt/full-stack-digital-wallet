2a63edf2cf744eb4bb0c2ff5190129c7c6537459
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 20:49:06 -0300

    feat: created AccountBalance component

--- frontend/src/components/AccountBalance.tsx
+++ frontend/src/components/AccountBalance.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import Button from './Button';
 
-const AccountBalance = () => (
-  <div>AccountBalance</div>
+type AccountBalanceType = {
+  balance: number;
+  updateBalance: Function;
+};
+
+const AccountBalance: FC<AccountBalanceType> = ({ balance, updateBalance }) => (
+  <form className="account-form">
+    {`R$ ${balance}`}
+
+    <Button
+      innerText="Update"
+      onClick={updateBalance}
+    />
+  </form>
 );
 
 export default AccountBalance;
