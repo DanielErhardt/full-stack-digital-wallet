b06b7a76e4126cab79bb4a44093252a1ffb78967
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 16:15:35 -0300

    feat: configured missing routes and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,27 @@ import {
 import NotFoundPage from './pages/NotFoundPage';
 import FormPage from './pages/FormPage';
 import HomePage from './pages/HomePage';
-import AccountPage from './pages/AccountPage';
 import TransferPage from './pages/TransferPage';
+import NGCashProvider from './context/NGCashProvider';
+import TransactionsPage from './pages/TransactionsPage';
+import DependentsPage from './pages/DependentsPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<FormPage />} />
-        <Route path="/register" element={<FormPage isRegister />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/transfer" element={<TransferPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <NGCashProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<FormPage />} />
+          <Route path="/register" element={<FormPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/dependents" element={<DependentsPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/transactions/:id" element={<TransactionsPage />} />
+          <Route path="/transfer" element={<TransferPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </NGCashProvider>
     </BrowserRouter>
   );
 }
