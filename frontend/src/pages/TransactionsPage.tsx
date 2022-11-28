c983c5f04fed5acb152ea364c979cfe34378b6d5
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 20:54:07 -0300

    fix: dependent transactions page it now fetched the correct transactions

--- frontend/src/pages/TransactionsPage.tsx
+++ frontend/src/pages/TransactionsPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Transaction } from '../@types/Transaction';
 import Header from '../components/Header';
+import List from '../components/List';
 import TransactionCard from '../components/TransactionCard';
 import { processTransactions } from '../helpers/processTransactions';
 import API from '../utils/API';
@@ -9,23 +10,22 @@ import API from '../utils/API';
 const TransactionsPage = () => {
   const [dependentUsername, setDependentUsername] = useState<string>('');
   const [dependentBalance, setDependentBalance] = useState<number>(0);
-  const [cashIn, setCashIn] = useState<Transaction[]>([]);
-  const [cashOut, setCashOut] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   const { id } = useParams();
 
   const fetchData = async () => {
-    const user = await API.getSelf();
-    const transactions = processTransactions(user);
-
     if (id) {
       const dependent = await API.getUserData(id);
+      const transactionsData = processTransactions(dependent);
       const { username, account: { balance } } = dependent;
       setDependentBalance(balance);
       setDependentUsername(username);
+      setTransactions(transactionsData);
+    } else {
+      const user = await API.getSelf();
+      const transactionsData = processTransactions(user);
+      setTransactions(transactionsData);
     }
-
-    setCashIn(transactions.filter((trn) => trn.type === 'cash in'));
-    setCashOut(transactions.filter((trn) => trn.type === 'cash out'));
   };
 
   useEffect(() => {
@@ -33,44 +33,26 @@ const TransactionsPage = () => {
   }, []);
 
   return (
-    <main>
+    <main className="list-page">
       <Header />
-      <div>
-        {id && (
-        <div>
-          <span>{`Dependent: ${dependentUsername}`}</span>
-          <span>{`Balance: ${dependentBalance}`}</span>
+      <h2>Transactions</h2>
+      {id && (
+        <div className="trn-dependents">
+          <p>{`Dependent: ${dependentUsername}`}</p>
+          <p>{`${dependentUsername}'s Balance: R$${dependentBalance}`}</p>
         </div>
-        )}
-      </div>
-      <div>
-        <p>Cash In:</p>
-        {cashIn.length === 0
-          ? 'You have no Cash In transactions.'
-          : cashIn.map((trn, i) => (
-            <TransactionCard
-              key={`trn${i}`}
-              otherUser={trn.otherUser}
-              date={trn.date}
-              value={trn.value}
-              type={trn.type}
-            />
-          ))}
-      </div>
-      <div>
-        <p>Cash Out:</p>
-        {cashOut.length === 0
-          ? 'You have no Cash Out transactions.'
-          : cashOut.map((trn, i) => (
-            <TransactionCard
-              key={`trn${i}`}
-              otherUser={trn.otherUser}
-              date={trn.date}
-              value={trn.value}
-              type={trn.type}
-            />
-          ))}
-      </div>
+      )}
+      <List>
+        {transactions.map((trn, i) => (
+          <TransactionCard
+            key={`trn${i}`}
+            otherUser={trn.otherUser}
+            date={trn.date}
+            value={trn.value}
+            type={trn.type}
+          />
+        ))}
+      </List>
     </main>
   );
 };
