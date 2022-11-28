990c80e952f361fb01a1b0fddca20577d758f522
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 16:07:28 -0300

    improve: made errorHandler more specific

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,15 +1,28 @@
 import { ErrorRequestHandler } from 'express';
 import codes from 'http-status-codes';
+import { ZodError } from 'zod';
 import RequestError from '../utils/RequestError';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   if (error instanceof RequestError) {
     const { statusCode, message } = error;
-    return res.status(statusCode).json({ requestError: message });
+    return res.status(statusCode).json({ message });
   }
 
-  return res.status(codes.INTERNAL_SERVER_ERROR).json(error);
+  if (error instanceof ZodError) {
+    const { issues } = error;
+    return res.status(codes.BAD_REQUEST).json({
+      message: 'Request has invalid properties. See error.issues for details.',
+      issues,
+    });
+  }
+
+  const { message, name, stack } = error as Error;
+
+  return res.status(codes.INTERNAL_SERVER_ERROR).json({
+    message, name, stack, completeError: error,
+  });
 };
 
 export default errorHandler;
