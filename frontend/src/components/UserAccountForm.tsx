a0c802612a2190f8b9d039a1e768eabf55973de7
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 16:16:57 -0300

    feat: created user account form component

--- frontend/src/components/UserAccountForm.tsx
+++ frontend/src/components/UserAccountForm.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { ChangeEventHandler, FC, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FormInput } from '../@types/FormInput';
+import useKeyPress from '../hooks/useKeyPress';
 
-function UserAccountForm() {
+type FormProps = {
+  onSubmit: () => void;
+  message: string;
+  isLogin: boolean;
+  form: FormInput;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+};
+
+const UserAccountForm: FC<FormProps> = (props) => {
+  const {
+    onSubmit, message, isLogin, form, handleChange,
+  } = props;
+  const enterPressed = useKeyPress('Enter');
+
+  useEffect(() => {
+    if (enterPressed) onSubmit();
+  }, [isLogin, enterPressed]);
+
   return (
-    <div>UserAccountForm</div>
+    <form>
+      <div>
+        <label htmlFor="username">
+          Username
+          <br />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={handleChange}
+            value={form.username}
+            placeholder="Your username here."
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={form.password}
+            placeholder="Your password here."
+          />
+        </label>
+      </div>
+      {message && (
+        <span>{message}</span>
+      )}
+      <div>
+        <button
+          type="button"
+          onClick={() => onSubmit()}
+        >
+          { isLogin ? 'LOGIN' : 'REGISTER' }
+        </button>
+        <br />
+        { isLogin && <Link to="/register">{'Don\'t have an account? Register now!'}</Link> }
+      </div>
+    </form>
   );
-}
+};
 
 export default UserAccountForm;
