ec08a83bcbb15299e11d8f4ab58697060eee939f
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-28 16:15:52 -0300

    feat: created header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import NGCashContext from '../context/NGCashContext';
 
-const Header = () => (
-  <div>Header</div>
-);
+const Header = () => {
+  const { username, logout } = useContext(NGCashContext);
+
+  return (
+    <header>
+      <span>
+        {`Hello, ${username}`}
+      </span>
+      <span>
+        <button
+          type="button"
+          onClick={() => logout()}
+        >
+          LOGOUT
+        </button>
+      </span>
+    </header>
+  );
+};
 
 export default Header;
